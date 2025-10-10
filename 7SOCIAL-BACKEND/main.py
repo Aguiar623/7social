69dad375f458c6ec776e7bc56bfb0830e3b7b5cb
Yeferson Aguiar <yeferson.aguiar@correounivalle.edu.co>
2025-10-10 14:36:08 -0500

    reconexion a git

--- 7SOCIAL-BACKEND/main.py
+++ 7SOCIAL-BACKEND/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173","http://localhost:5173"],
+    allow_origins=["https://7social.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
